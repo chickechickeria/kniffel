fa942d9a9743c7fe0d05165b58f3a9253df6be9e
maikesie <maike.siebelds@gmail.com>
2015-10-22 15:01:08 +0200

    Punktetabelle

---
+++ maike.js
@@ -0,0 +1,37 @@
+var w1 = new Image();
+ w1.src = "/bilder/Einser.png";
+ w1.height = "50px";
+ w1.width = "50px"
+ w1.alt  = "Würfel1";
+
+var w2 = new Image();
+ w2.src = "/bilder/Zweier.png";
+ w2.height = "50px";
+ w2.width = "50px"
+ w2.alt  = "Würfel2";
+
+var w3 = new Image();
+ w3.src = "/bilder/Dreier.png";
+ w3.height = "50px";
+ w3.width = "50px"
+ w3.alt  = "Würfel1";
+
+var w1 = new Image();
+ w1.src = "/bilder/Vierer.png";
+ w1.height = "50px";
+ w1.width = "50px"
+ w1.alt  = "Würfel1";
+
+var w1 = new Image();
+ w1.src = "/bilder/Fuenfer.png";
+ w1.height = "50px";
+ w1.width = "50px"
+ w1.alt  = "Würfel1";
+
+var w1 = new Image();
+ w1.src = "/bilder/Einser.png";
+ w1.height = "50px";
+ w1.width = "50px"
+ w1.alt  = "Würfel1";
+
+ 
